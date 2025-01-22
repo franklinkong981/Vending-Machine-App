67e9a297f4babe3afadb7cbea9cdfcfd0ea9420e
Franklin Kong <franklinkong981@gmail.com>
2025-01-22 10:20:06 -0800

    Set up starter code and created VendingMachine component

---
+++ vending-machine/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+import VendingMachine from "./VendingMachine.jsx";
+
+function App() {
+
+  return (
+   <VendingMachine />
+  );
+}
+
+export default App;
