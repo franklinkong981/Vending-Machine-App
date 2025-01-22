521f7d98c05dbb99b2aeab020c7b1d9e94eae082
Franklin Kong <franklinkong981@gmail.com>
2025-01-22 10:32:33 -0800

    Got different components and main BrowserRouter done

--- vending-machine/src/App.jsx
+++ vending-machine/src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
 
 import VendingMachine from "./VendingMachine.jsx";
+import Chips from "./Chips.jsx";
+import Soda from "./Soda.jsx";
+import Candy from "./Candy.jsx";
 
 function App() {
 
   return (
-   <VendingMachine />
+   <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<VendingMachine />} />
+        <Route path="/chips" element={<Chips />} />
+        <Route path="/soda" element={<Soda/>} />
+        <Route path="/candy" element={<Candy/>} />
+      </Routes>
+    </BrowserRouter>
+   </div>
   );
 }
 
---
+++ vending-machine/src/Soda.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Soda = () => {
+
+};
+
+export default Soda;
