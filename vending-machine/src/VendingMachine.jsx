18d8f3daaa2ec26d9d9e439934470fc42027b52d
Franklin Kong <franklinkong981@gmail.com>
2025-01-22 10:37:24 -0800

    Added links on homepage to different snacks

--- vending-machine/src/VendingMachine.jsx
+++ vending-machine/src/VendingMachine.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 const VendingMachine = () => {
-  
+  return (
+    <div className="VendingMachine">
+      <h1 className="VendingMachine-welcome">Welcome to the Vending Machine</h1>
+      <h2 className="VendingMachine-instructions">Please select a snack below</h2>
+      <div className="VendingMachine-links">
+        <Link to="/chips">Chips</Link>
+        <Link to="/soda">Soda</Link>
+        <Link to="/candy">Candy</Link>
+      </div>
+    </div>
+  );
 };
 
 export default VendingMachine;
