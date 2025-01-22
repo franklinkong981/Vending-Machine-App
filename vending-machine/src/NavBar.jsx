b89f993b415517f6c9da0a1837b6c55c0273386d
Franklin Kong <franklinkong981@gmail.com>
2025-01-22 11:06:36 -0800

    Styled active, pending, and inactive navlinks

--- vending-machine/src/NavBar.jsx
+++ vending-machine/src/NavBar.jsx
@@ -1,33 +1,35 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
 
+import "./NavBar.css";
+
 const NavBar = () => {
   return (
     <nav className="NavBar">
       <NavLink to="/"
         className={({ isActive, isPending }) =>
-          isPending ? "pending" : isActive ? "active" : ""
+          isPending ? "pending" : isActive ? "active" : "inactive"
         }
       >
         Home
       </NavLink>
       <NavLink to="/chips"
         className={({ isActive, isPending }) =>
-          isPending ? "pending" : isActive ? "active" : ""
+          isPending ? "pending" : isActive ? "active" : "inactive"
         }
       >
         Chips
       </NavLink>
       <NavLink to="/soda"
         className={({ isActive, isPending }) =>
-          isPending ? "pending" : isActive ? "active" : ""
+          isPending ? "pending" : isActive ? "active" : "inactive"
         }
       >
         Soda
       </NavLink>
       <NavLink to="/candy"
         className={({ isActive, isPending }) =>
-          isPending ? "pending" : isActive ? "active" : ""
+          isPending ? "pending" : isActive ? "active" : "inactive"
         }
       >
         Candy
